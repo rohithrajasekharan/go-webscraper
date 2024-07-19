66a59ac878d0b1829c20fb0467d88dfdd03e12f1
rohithrajasekharan <rohithrajasekharan@outlook.com>
2024-07-19 12:34:28 +0530

    http request and response json formatting in utils

---
+++ utils/utils.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func ParseJSON(req *http.Request, payload any) error {
+	if req.Body == nil {
+		return fmt.Errorf("missing request body")
+	}
+	return json.NewDecoder(req.Body).Decode(payload)
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(v)
+}
+
+func WriteError(w http.ResponseWriter, status int, err error) {
+	WriteJSON(w, status, map[string]string{"error": err.Error()})
+}
